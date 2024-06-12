ee0911c294d1471ecefa79181503972f1bd26f63
Douglas <douglas-brumley@hotmail.com>
2024-06-12 18:57:02 -0400

    Excercise Completed

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car car1 = new Car();
+            car1.Make = "BMW";
+            car1.Model = "X2";
+            car1.Year = 2025;
+
+            Console.WriteLine($"{car1.Make}, {car1.Model}, {car1.Year}");
+
         }
+
+
     }
+
+
 }
+
+
